71c695801b99b5dab76b4277f8b1c35871abf387
Davi Maciel Ferreira <104574671+DfMaciel@users.noreply.github.com>
2024-09-03 12:21:46 -0300

    Base da tarefa criada

---
+++ src/conexao.py
@@ -0,0 +1,15 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+def conexao ():
+    uri = "mongodb+srv://DaviMaciel:davimaciel2@cluster0.aggpb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    global db 
+    db = client.Cluster0
+
+    try:
+        client.admin.command('ping')
+        print("Conexão realizada!")
+    except Exception as e:
+        print(e)
